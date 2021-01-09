7796a991b4b984f6f008b4e8844882217fcdc6dd
Dan <danazad@gmail.com>
2021-01-08 23:30:52 -0500

    Made currencies work

--- currencyConverter.js
+++ currencyConverter.js
@@ -1,3 +1,13 @@
+function dollarToDollar() {
+    let output = 0;
+
+    $("#input").each(function() {
+        output += Number($(this).val());
+    })
+
+    $("#output").val(`${(output * 1).toFixed(2)}`);
+}
+
 function dollarToPound() {
     let output = 0;
 
@@ -6,6 +16,26 @@ function dollarToPound() {
     })
 
     $("#output").val(`${(output * .74).toFixed(2)}`);
+}
+
+function dollarToEuro() {
+    let output = 0;
+
+    $("#input").each(function() {
+        output += Number($(this).val());
+    })
+
+    $("#output").val(`${(output * .84).toFixed(2)}`);
+}
+
+function dollarToRial() {
+    let output = 0;
+
+    $("#input").each(function() {
+        output += Number($(this).val());
+    })
+
+    $("#output").val(`${(output * 42105).toFixed(2)}`);
 }
 
 function poundToDollar() {
@@ -18,32 +48,143 @@ function poundToDollar() {
     $("#output").val(`${(output * 1.34).toFixed(2)}`);
 }
 
+function euroToPound() {
+    let output = 0;
+
+    $("#input").each(function() {
+        output += Number($(this).val());
+    })
+
+    $("#output").val(`${(output * 0.90).toFixed(2)}`);
+}
+
 $(document).ready(function() {
-    $("input").keyup(function() {
+    $("#input").keyup(function() {
         dollarToPound()
     })
 
-    let currencyFunctions = "pound"; //This is for keeping track of what the function should do the following
+    let currencyFunctions = "currencyOne"; //This is for keeping track of what the function should do the following
 
     $("#switch").click(function() {
-        if (currencyFunctions == "pound") {
+        if (currencyFunctions == "currencyOne") {
             poundToDollar()
             $("#sign-input").html("£");
             $("#sign-output").html("$");
-            $("#currency-one").html("Pounds");
-            $("#currency-two").html("Dollars");
-            currencyFunctions = "dollar";
+            $("#input").attr('placeholder', 'Pounds');
+            currencyFunctions = "currencyTwo";
             return;
         }
         
-        if (currencyFunctions == "dollar") {
+        if (currencyFunctions == "currencyTwo") {
             dollarToPound()
             $("#sign-input").html("$");
             $("#sign-output").html("£");
-            $("#currency-one").html("Dollars");
-            $("#currency-two").html("Pounds");
-            currencyFunctions = "pound";
+            $("#input").attr('placeholder', 'Dollars');
+            currencyFunctions = "currencyOne";
             return;
+        }
+    });
+
+    $("#currency-input").click(function() {
+        let currency = $(this).val();
+        let currencyOut = $("#currency-output").val();
+
+        $("#sign-input").html(currency);
+
+        if (currency == "$" && currencyOut == "$") {
+            $("#input").val('');
+            $("#output").val('');
+
+            $("#input").keyup(function() {
+                dollarToDollar();
+            })
+        } else if (currency == "$" && currencyOut == "£") {
+            $("#input").val('');
+            $("#output").val('');
+
+            $("#input").keyup(function() {
+                dollarToPound();
+            })
+        } else if (currency == "€") {
+            $("#input").val('');
+            $("#output").val('');
+
+            $("#input").keyup(function() {
+                dollarToEuro();
+            })
+        } else {
+            $("#input").val('');
+            $("#output").val('');
+
+            $("#input").keyup(function() {
+                dollarToRial();
+            }) 
+        }
+    });
+
+    $("#currency-output").click(function() {
+        let currency = $(this).val();
+
+        $("#sign-output").html(currency);
+
+        if (currency == "$") {
+            $("#input").val('');
+            $("#output").val('');
+
+            $("#input").keyup(function() {
+                dollarToDollar();
+            })
+        } else if (currency == "£") {
+            $("#input").val('');
+            $("#output").val('');
+
+            $("#input").keyup(function() {
+                dollarToPound();
+            })
+        } else if (currency == "€") {
+            $("#input").val('');
+            $("#output").val('');
+
+            $("#input").keyup(function() {
+                dollarToEuro();
+            })
+        } else {
+            $("#input").val('');
+            $("#output").val('');
+
+            $("#input").keyup(function() {
+                dollarToRial();
+            }) 
+        }
+    });
+
+    $("#currency-input").click(function() {
+        let currency = $(this).val();
+        $("#sign-input").html(currency);
+
+        if (currency == "$") {
+            $("#input").attr('placeholder', 'Dollars')
+        } else if (currency == "£") {
+            $("#input").attr('placeholder', 'Pounds')
+        } else if (currency == "€") {
+            $("#input").attr('placeholder', 'Euros')
+        } else {
+            $("#input").attr('placeholder', 'Rials')
+        }
+    });
+
+    $("#currency-output").click(function() {
+        let currency = $(this).val();
+        $("#sign-output").html(currency);
+
+        if (currency == "$") {
+            $("#output").attr('placeholder', 'Dollars')
+        } else if (currency == "£") {
+            $("#output").attr('placeholder', 'Pounds')
+        } else if (currency == "€") {
+            $("#output").attr('placeholder', 'Euros')
+        } else {
+            $("#output").attr('placeholder', 'Rials')
         }
     });
 });
